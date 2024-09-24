b92aa97855e59d79e77d59ffe71d8aa3218ff57e
ANNA MARONE <annamarone72@gmail.com>
2024-09-24 14:03:14 +0000

    integration de la page connexion et inscription

--- src/components/pages/Auth/Login.jsx
+++ src/components/pages/Auth/Login.jsx
@@ -1,13 +1,70 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-const Login = () => {
-  return (
-    <div>
-      Login
-      <NavLink to="/register">créer un compte</NavLink>
-    </div>
-  )
-}
+import './login.css';
+import AuthUser from './AuthUser';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-export default Login
+export default function Login() {
+  const { http, setToken } = AuthUser();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const submitForm = () => {
+    // API call
+    http.post('/login', { email: email, password: password }).then((res) => {
+      setToken(res.data.user, res.data.access_token);
+    });
+  };
+
+  return (
+    <div className='bannier_connexion'>
+      <div className="section_form">
+        <div className="col-sm-6">
+          <div className="contenu">
+            <div className='titre_des'>
+              <h1>Bienvenue sur <span>SénAgri</span></h1>
+            </div>
+            <h1>Se connecter</h1>
+            <div className="form-group">
+              <label>Entre votre adresse email: </label><br />
+              <div className="input-icon">
+                <i className="fas fa-envelope"></i>
+                <input 
+                  type="email" 
+                  placeholder="example@gmail.com"
+                  onChange={e => setEmail(e.target.value)}
+                  id="email" 
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Entre votre mot de passe: </label><br />
+              <div className="input-icon">
+                <i className="fas fa-lock"></i>
+                <input 
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="********"
+                  onChange={e => setPassword(e.target.value)}
+                  id="pwd" 
+                />
+                <i 
+                  className={`fas ${showPassword ? 'fa-eye-slash' : 'fa-eye'} password-toggle`}
+                  onClick={togglePasswordVisibility}
+                  style={{ cursor: 'pointer'}}
+                ></i>
+              </div>
+            </div>
+            <p>Pas de compte ?<NavLink to="/register" className='span'>S&apos;inscrire</NavLink></p>
+            <button type="button" onClick={submitForm}>Se connecter</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/pages/Auth/Register.jsx
+++ src/components/pages/Auth/Register.jsx
@@ -1,12 +1,126 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { NavLink } from 'react-router-dom';
+import './registe.css';
+import AuthUser from './AuthUser';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import profil from '/src/assets/images/2579132.png'
 
-const Register = () => {
+export default function Register(){
+  const { http, setToken } = AuthUser();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const [profileImage, setProfileImage] = useState(null);
+  const fileInputRef = useRef(null);
+
+  // Fonction pour gérer le changement d'image
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfileImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // Fonction pour déclencher l'input file en cliquant sur l'image
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+ 
   return (
-    <div>
-      register
+    <div className='bannier_connexion'>
+      <div className="section_form">
+        <div className="col-sm-6">
+          <div className="contenu">
+            <h1>S&apos;inscrire</h1>
+       {/* image */}
+       <div className='profil'>
+      <div className="input-icon">
+        {/* Affichage de l'image de profil (par défaut ou nouvelle image) */}
+        <img 
+          src={profileImage || profil} 
+          alt="Profil" 
+          style={{ width: '100px', height: '100px', borderRadius: '50%', cursor: 'pointer' }} 
+          onClick={handleImageClick} 
+        />
+        <input 
+          type="file" 
+          id="profile-upload" 
+          ref={fileInputRef} 
+          style={{ display: 'none' }} 
+          accept="image/*" 
+          onChange={handleImageChange}
+        />
+      </div>
     </div>
-  )
+    {/* ============================== */}
+            <div className='from_group1'>
+            <div className="form-group">
+              <label>Prenom et nom: </label><br />
+                <input type="text" placeholder="entre votre nom" id="name" />
+            </div>
+            <div className="form-group">
+              <label>Adresse: </label><br />
+                <input type="text" placeholder="entre votre adresse" id="adresse" />
+            </div>
+           
+            </div>
+            <div className="form-group">
+              <label>Numéro de téléphone: </label><br />
+                <input type="text" placeholder="entre votre numéro de tél" id="telephone" />
+            </div>
+            <div className="form-group">
+              <label>Entre votre adresse email: </label><br />
+              <div className="input-icon">
+                <i className="fas fa-envelope"></i>
+                <input 
+                  type="email" 
+                  placeholder="example@gmail.com"
+                  
+                  id="email" 
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Entre votre mot de passe: </label><br />
+              <div className="input-icon">
+                <i className="fas fa-lock"></i>
+                <input 
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="********"
+                  onChange={e => setPassword(e.target.value)}
+                  id="pwd" 
+                />
+                <i 
+                  className={`fas ${showPassword ? 'fa-eye-slash' : 'fa-eye'} password-toggle`}
+                  onClick={togglePasswordVisibility}
+                  style={{ cursor: 'pointer'}}
+                ></i>
+              </div>
+              <div className="form-group">
+              <label>acteur: </label><br />
+              <select name="acteur" id="">
+                <option value="client">client</option>
+                <option value="producteur">producteur</option>
+              </select>
+            </div>
+            </div>
+            <p>j&apos;ai déjà un compte?<NavLink to="/login" className='span'>S&apos;inscrire</NavLink></p>
+            <button type="button" >Se connecter</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+
