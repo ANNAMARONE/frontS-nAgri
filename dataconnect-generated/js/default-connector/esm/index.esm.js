d06d8d0dabb858227140d42ed843757b6dc428b0
ANNAMARONE <annamarone72@gmail.com>
2024-11-16 20:02:56 +0000

    modification du css

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'SenAgriF',
-  location: 'us-central1'
-};
-
