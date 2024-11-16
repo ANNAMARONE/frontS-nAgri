f02ddffdfee528169216792a92c8d3ad77032a09
ANNA MARONE <annamarone72@gmail.com>
2024-11-16 15:14:36 +0000

    Configure Firebase Hosting GitHub Actions workflows

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'SenAgriF',
+  location: 'us-central1'
+};
+
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src', 
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], 
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, 
   },
 })
